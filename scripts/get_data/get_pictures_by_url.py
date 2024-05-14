22468a641f14059a8f0d38fec91e450778f23832
nvanderperren <nastasia.vanderperren@meemoo.be>
2024-05-14 17:43:19 +0200

    add script to scrape pictures

---
+++ scripts/get_data/get_pictures_by_url.py
@@ -0,0 +1,26 @@
+import requests
+from csv import DictReader
+from sys import argv
+from time import sleep
+
+source_file = argv[1]
+FOLDER = argv[2]
+
+def get_picture(url: str, id: str, session: requests.Session) -> None:
+    image = session.get(url).content
+    print("getting picture {}".format(url))
+    with open('{}/{}.jpg'.format(FOLDER, id), 'wb') as handler:
+        handler.write(image)
+
+with open(source_file, 'r') as source:
+    reader = DictReader(source)
+    with requests.Session() as session:
+        for row in reader:
+            id = row['URI'].split('/')[-1]
+            url = row['foto']
+            print("busy with {}".format(id))
+            if url:
+                get_picture(url, id, session)
+            else:
+                print('no picture')
+            sleep(3)
