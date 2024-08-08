27805331b6863c2f7d2701de1ea62261a73d4cab
nvanderperren <nastasia.vanderperren@meemoo.be>
2024-08-08 12:02:54 +0200

    refactor commons script to general wiki script

--- scripts/get_data/get_wiki_pics.py
+++ scripts/get_data/get_wiki_pics.py
@@ -3,6 +3,7 @@ from sys import argv
 from csv import DictReader
 from pathlib import Path
 import shutil
+import os
 
 source_file = argv[1]
 output_folder = argv[2]
@@ -22,19 +23,35 @@ def download_category(download_path, name):
         except Exception as error:
             print(error)
 
+def download_image(download_path, image):
+    image_path = "File:" + image
+    output_path = "{}/{}".format(download_path, image)
+    command = 'wikiget \"{}\" -o \"{}\"'.format(image_path, output_path)
+    print("downloading " + image)
+    os.system(command)
+
+
 
 if __name__ == '__main__':
     with open(source_file, 'r') as csv_file:
         reader =  DictReader(csv_file)
+        
         for row in reader:
             category = row["Commons category"]
-            if category:
-                print(category)
+            image = row["image"]
+            
+            if category or image:
                 print("busy with " + row["volledige naam"])
                 download_path = "{}/{}".format(output_folder, row['Wikidata ID'])
                 Path(download_path).mkdir(parents=True, exist_ok=True)
-                download_category(download_path, category)
-                print("done\n")
+
+                if category:
+                    download_category(download_path, category)
+
+                if image:
+                    download_image(download_path, image)
+  
+            print("done\n")
 
 
 shutil.rmtree('apicache')
